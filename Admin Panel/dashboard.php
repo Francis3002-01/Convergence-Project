9bd93a412bb65bf476625501df9275bcbb9693a0
John Francis <francisorevillo66@gmail.com>
2026-09-04 00:36:15 +0800

    made some changes

---
+++ Admin Panel/dashboard.php
@@ -0,0 +1,8 @@
+/*
+Dashboard has:
+-number of journal downloads
+-number of journal downloads by continent
+-percentage of journal downloads by continent by graph
+
+Code coming soon...
+*/
---
+++ Admin Panel/manage_journal.php
@@ -0,0 +1,14 @@
+/*
+    Manage Journals has:
+    - Search bar
+    -Add journal
+    -Edit journal
+    -Delete journal
+    -Journal Details
+    -Save Draft
+    -Download journal
+    -Read Journal Online
+
+    Code coming soon...
+*/
+
