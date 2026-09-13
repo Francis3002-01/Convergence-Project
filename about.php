bbfc58c20df09d363df48f10159400200066df66
John Francis <francisorevillo66@gmail.com>
2026-09-13 15:06:27 +0800

    updated homepage

--- about.php
+++ about.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage | Convergence</title>
+    <title>About | Convergence</title>
     
     <!--External CSS-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" href="includes_css/header.css">
+    <link rel="stylesheet" href="css/transition.css">
     <link rel="stylesheet" href="includes_css/footer.css">
     <link rel="stylesheet" href="css/about_page.css">
 </head>
